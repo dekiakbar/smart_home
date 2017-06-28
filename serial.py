c9d07010e460ba67b431a6bfddcb24a3f71f7314
deki <dekiakbar@linuxmail.org>
2017-06-28 20:30:11 +0700

    update

---
+++ serial.py
@@ -0,0 +1,12 @@
+import serial 
+
+device = '/dev/ttyACM0' 
+try:
+  arduino = serial.Serial(device, 38400) 
+except: 
+  print "Failed to connect on",device    
+try: 
+  data = arduino.readline()  
+  print data  
+except:
+  print "Failed to get data from Arduino!"
