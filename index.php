2d07d7468c46dbf4323ef0fafb4b00c65faf7a56
deki <dekiakbar@linuxmail.org>
2017-07-05 00:18:25 +0700

    fix layout,gambar dan design

--- index.php
+++ index.php
@@ -10,180 +10,301 @@
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
   <link rel="stylesheet" type="text/css" href="assets/css/materialize.min.css" />
   <link rel="stylesheet" type="text/css" href="assets/css/materialdesignicons.min.css" />
-  <link rel="stylesheet" type="text/css" href="assets/css/arduino-style.css" />
   <link rel="stylesheet" type="text/css" href="assets/css/sweetalert.css">
   
   <script type="text/javascript" src="assets/js/jQuery-2.1.4.min.js"></script>
   <script type="text/javascript" src="assets/js/jquery.form.min.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <style type="text/css">
+    html, body {
+  background: url(assets/img/background.png) no-repeat;
+  background-size: cover;
+  font-family: 'Lato', Calibri, Arial, sans-serif;
+}
+.arduino-header {
+  position: absolute;
+  margin: 4em 30px;
+}
+.arduino-header img {
+  margin: 3em 0;
+  width: 450px;
+}
+.arduino-body {
+  position: absolute;
+  margin: 9em -3em;
+}
+.icon img{
+  width: auto;
+  height: 40px;
+}
+.arduino-body-header img {
+  margin-left: 4em;
+  width: auto;
+  height: 65px;
+}
+.arduino-body .tbl-header {
+  margin-bottom: 15px;
+  font-size: 23px;
+  font-weight: 500;
+}
+.arduino-body table {
+  position: absolute;
+  width: 500px;
+  height: 600px;
+  background: url(assets/img/layout.png) no-repeat;
+  background-size: cover;
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.1);
+}
+.arduino-body tbody {
+  position: absolute;
+  margin: 7em 20px;
+}
+.arduino-body thead {
+  position: absolute;
+  margin-left: 10px;
+}
+.arduino-body tbody .result-body {
+  position: absolute;
+  margin-left: -12em;
+  background: url(assets/img/element/nan.nan.png) no-repeat;
+  width: 120px;
+  height: 60px;
+}
+.arduino-body .refresh {
+  margin: 10px 9em;
+}
+.arduino-switch {
+    position: absolute;
+    margin: 6.7em -4em;
+    left: 58%;
+    font-size: 20px;
+    font-weight: 500;
+}
+.arduino-switch-header img {
+  margin-left: 10px;
+  width: auto;
+  height: 65px;
+}
+.arduino-switch table {
+  position: absolute;
+  margin-top: 0em;
+  width: 480px;
+  height: 500px;
+  background: url(assets/img/layout.png) no-repeat;
+  background-size: cover;
+}
+.arduino-switch thead {
+  position: absolute;
+  margin: 0 10px;
+}
+.arduino-switch tbody {
+  position: absolute;
+  margin: 6em 2em;
+}
+.arduino-switch .label-relay {
+  font-size: 25px;
+}
+.arduino-img img{
+  height: auto;
+  width: 300px;
+  float: right;
+  margin-top:180px;
+}
+.arduino-logo img{
+  height: 100px;
+  width: auto;
+  float: right;
+  margin: 20px -200px auto auto;
+}
+.arduino-header {
+  margin-top: -40px;
+  padding: 0;
+}
+footer {
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  height: 70px;
+  background: url(assets/img/footer-bar.png) no-repeat;
+  color:#000;
+  line-height: 70px;
+}
+footer a {
+  color: #ffffff;
+}
+.footer-copyright {
+  margin-left: 10px;
+  background: url(assets/img/footer-bar.png) no-repeat;
+  color: #ffffff;
+  font-size: 20px;
+}
+.footer-copyright img {
+  position: absolute;
+  margin: -16px;
+  left: 67%;
+}
+.switch label input[type=checkbox]:checked + .lever {
+  background-color: #A5D6A7;
+  height: 70px;
+}
+
+.switch label input[type=checkbox]:checked + .lever:after {
+  background-color: #81C784;
+  left: 24px;
+}
+  </style>
+
 </head>
 <body>
-  <div class="arduino-img">
-    <img src="assets/img/icon/03.png">
-  </div>
   <div class="arduino-header">
-     <img src="assets/img/icon/02.png">
-	 <h5 class="blue-text text-darken-1">Arduino</h5>
-	 <span class="red-text text-lighten-2">Web Controll</span>
-  </div>
-  <div class="arduino-logo">
-    <img src="assets/img/icon/rpiardu.jpg">
+    <img src="assets/img/logo.png">
   </div>
 
   <div class="container">
-  	<div class="arduino-body">
-        <div class="tbl-header green-text text-darken-1"><i class="mdi mdi-table-large"></i> Table Data Controll 
-          <button type="nutton" id="add-more" class="refresh waves-effect eaves-light btn right green lighten-1"><i class="mdi mdi mdi-refresh mdi-32px right"></i> Refresh Data</button>
-        </div>
-          <table class="stiped centered">
+    <div class="arduino-body">
+          <table>
              <thead>
-      	         <tr>
-      		        <th>Device</th>
-      		        <th>State</th>
-      	         </tr>
+                 <tr>
+                  <th><i class="arduino-body-header"><img src="assets/img/element/Data-device-LOGO.png"></i></th>
+                 </tr>
              </thead>
 
              <tbody>
                   <tr>
-                   <td>Temperature</td>
-                   <td><div id="temp"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-temp.png"></i></td>
+                   <td class="result-body"><div id="temp"></div></td>
                   <td></td>
                  </tr>
                  <tr>
-                   <td>Humidity</td>
-                   <td><div id="kelembaban"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-hum.png"></i></td>
+                   <td class="result-body"><div id="kelembaban"></div></td>
                   <td></td>
                  </tr>
                  <tr>
-                   <td>Rain Sensor</td>
-                   <td><div id="hujan"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-light.png"></i></td>
+                   <td class="result-body"><div id="hujan"></div></td>
                   <td></td>
                  </tr>
                  <tr>
-                   <td>Light Sensor</td>
-                   <td><div id="cahaya"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-wat.png"></i></td>
+                   <td class="result-body"><div id="cahaya"></div></td>
                   <td></td>
                  </tr>
                  <tr>
-                   <td>Garage Door</td>
-                   <td><div id="pintu"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-gar.png"></i></td>
+                   <td class="result-body"><div id="pintu"></div></td>
                   <td></td>
                  </tr>
                   <tr>
-                   <td>Water Sensor</td>
-                   <td><div id="air"></div></td>
+                   <td><i class="icon"><img src="assets/img/element/Data-device-rain.png"></i></td>
+                   <td class="result-body"><div id="air"></div></td>
                   <td></td>
                  </tr>
+                 <tr>
+                  <td> 
+                    <button type="button" id="add-more" class="refresh waves-effect eaves-light btn blue darken-4" style="width:200px;"><i class="mdi mdi mdi-refresh mdi-32px right"></i> Refresh Data</button>
+                  </td>
+                </tr>
              </tbody>
         </table>
     </div>
  </div>
 
   <div class="arduino-switch">
-    <div class="swc-header red-text text-darken-2"><i class="mdi mdi-rotate-315 mdi-xbox-controller mdi-32px"></i> Switch Controll</div>
      <form id="myForm" name="myform" >
-      <table class="centered responsive-table">
+      <table class="responsive-table">
          <thead>
-           <tr>
-              <th>Relay 1</th>
-              <th>Relay 2</th>
-              <th>Relay 3</th>
+           <th><i class="arduino-switch-header"><img src="assets/img/element/switch-control relay-01-LOGO.png"></i></th>
           </thead>
           <tbody>
-          	<tr>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r1" class="relay" name="relay1" data-name="relay1">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r2" class="relay"  name="relay2" data-name="relay2">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r3" class="relay"  name="relay3" data-name="relay3">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
+            <tr>
+              <td class="label-relay">Relay 1</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r1" class="relay" name="relay1" data-name="relay1">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
+              <td class="label-relay">Relay 2</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r2" class="relay"  name="relay2" data-name="relay2">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
             </tr>
-        </tbody>
-
-         <thead>
-           <tr>
-              <th>Relay 4</th>
-              <th>Relay 5</th>
-              <th>Relay 6</th>
-          </thead>
-          <tbody>
-          	<tr>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r4" class="relay"  name="relay4" data-name="relay4">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r5" class="relay"  name="relay5" data-name="relay5">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r6" class="relay"  name="relay6" data-name="relay6">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
+            <tr>
+              <td class="label-relay">Relay3</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r3" class="relay"  name="relay3" data-name="relay3">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
+              <td class="label-relay">Relay 4</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r4" class="relay"  name="relay4" data-name="relay4">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
             </tr>
-        </tbody>
-
-         <thead>
-           <tr>
-              <th>Relay 7</th>
-              <th>Relay 8</th>
-          </thead>
-          <tbody>
-          	<tr>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r7" class="relay"  name="relay7" data-name="relay7">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
-          		<td>
-          		   <div class="switch">
-          		   	 <label>Off
-          		   	   <input type="checkbox" id="r8" class="relay"  name="relay8" data-name="relay8">
-          		   	   <span class="lever"></span>
-          		   	   On
-          		     </labekl>
-          		   </div>
-          		</td>
+            <tr>
+              <td class="label-relay">Relay 5</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r5" class="relay"  name="relay5" data-name="relay5">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
+              <td class="label-relay">Relay 6</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r6" class="relay"  name="relay6" data-name="relay6">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
+            </tr>
+            <tr>
+              <td class="label-relay">Relay 7</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r7" class="relay"  name="relay7" data-name="relay7">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
+              <td class="label-relay">Relay 8</td>
+              <td>
+                 <div class="switch">
+                   <label>Off
+                     <input type="checkbox" id="r8" class="relay"  name="relay8" data-name="relay8">
+                     <span class="lever"></span>
+                     On
+                   </labekl>
+                 </div>
+              </td>
             </tr>
         </tbody>
       </table>
@@ -199,8 +320,9 @@
 <script type="text/javascript" src="assets/js/arduino-javascript.js"></script>
 </body>
 <footer>
-    <div class="footer-copyright" style="text-align: center">
-            © <?php echo date('Y') ?> <a href="#">Divisi Ristek HMTI</a> All rights reserved
+    <div class="footer-copyright">
+            <?php echo date('Y') ?> © <a href="#">dept..ristek.hmti</a> All rights reserved
+            <img src="assets/img/footer-02.png">
     </div>
 </footer>
 </html>
